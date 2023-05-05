95346cb9c9e6414abcc573b040dbfcfb4d899f4b
Lekuru <levijordan007@gmail.com>
2023-05-05 23:15:44 +0200

    Create database service

--- bancho/services.py
+++ bancho/services.py
@@ -1,11 +1,20 @@
 
+from .common.database import Postgres
 from .logging import ConsoleHandler
 from .server import BanchoFactory
 
 import logging
+import config
 
 logger = logging.getLogger('anchor')
 logger.setLevel(logging.DEBUG)
 logger.addHandler(ConsoleHandler)
 
 factory = BanchoFactory()
+
+database = Postgres(
+    config.POSTGRES_USER,
+    config.POSTGRES_PASSWORD,
+    config.POSTGRES_HOST,
+    config.POSTGRES_PORT
+)
