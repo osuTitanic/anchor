4215325f7d3398836901c2fe0a5a3de487e263e5
Lekuru <levijordan007@gmail.com>
2023-05-05 21:28:35 +0200

    Add "Status" object for player

--- bancho/objects/player.py
+++ bancho/objects/player.py
@@ -1,5 +1,25 @@
 
+from dataclasses import dataclass, field
+from typing      import List
+
 from ..protocol import BanchoProtocol
+
+from ..constants import (
+    ClientStatus,
+    Mode,
+    Mod
+)
+
+@dataclass
+class Status:
+    action: ClientStatus = ClientStatus.Idle
+    text: str = ""
+    checksum: str = ""
+    mods: List[Mod] = field(default_factory=list) # = []
+    mode: Mode = Mode.Osu
+
+    def __repr__(self) -> str:
+        return f'<Status ({self.action})>'
 
 class Player(BanchoProtocol):
     pass
