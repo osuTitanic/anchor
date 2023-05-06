784698b40f3d3b8b69cd13936327a6c934bf010f
Lekuru <levijordan007@gmail.com>
2023-05-06 23:56:44 +0200

    Implement login reply

--- bancho/handlers/b20130606.py
+++ bancho/handlers/b20130606.py
@@ -1,6 +1,21 @@
 
+from bancho.constants import RequestPacket, ResponsePacket
+from bancho.streams   import StreamIn, StreamOut
+
 from . import BaseHandler
 
+import bancho
+
 class b20130606(BaseHandler):
-    def login_success(self):
+    def login_reply(self, response: int):
+        self.player.sendPacket(
+            ResponsePacket.LOGIN_REPLY,
+            int(response).to_bytes(
+                length=4,
+                byteorder='little',
+                signed=True
+            )
+        )
+
+    def send_message(self, sender, message: str, target_name: str):
         pass
