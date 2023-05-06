6c7172064adf99ff9136834d1319e5a7c83a689d
Lekuru <levijordan007@gmail.com>
2023-05-06 23:55:54 +0200

    Load channels on startup

--- bancho/server.py
+++ bancho/server.py
@@ -4,6 +4,7 @@ from twisted.internet.interfaces import IAddress
 from typing import Optional
 
 from .objects.player import Player
+from .objects.channel import Channel
 
 import bancho
 
@@ -11,10 +12,21 @@ class BanchoFactory(Factory):
     protocol = Player
 
     def startFactory(self):
+        bancho.services.logger.info('Loading channels...')
+
+        # Loading channels
+        for channel in bancho.services.database.channels():
+            bancho.services.logger.info(f'- {channel.name}')
+            bancho.services.channels.append(
+                Channel.from_db(channel)
+            )
+
         bancho.services.logger.info(f'Starting factory: {self}')
 
     def stopFactory(self):
         bancho.services.logger.warning(f'Stopping factory: {self}')
+
+        # TODO: Disconnect all players
 
     def buildProtocol(self, addr: IAddress) -> Optional[Protocol]:
         client = self.protocol(addr)
