cb611761a60149abb6266d0c0b8a40f82a3bb624
Lekuru <levijordan007@gmail.com>
2023-05-07 20:58:41 +0200

    Add enqueue_player method for USER_PRESENCE_SINGLE

--- bancho/objects/collections.py
+++ bancho/objects/collections.py
@@ -7,7 +7,8 @@ from typing import (
     Set
 )
 
-from .channel import Channel
+from ..constants import ResponsePacket
+from .channel    import Channel
 
 class Channels(List[Channel]):
     def __iter__(self) -> Iterator[Channel]:
@@ -55,7 +56,7 @@ class Players(List[Player]):
         return {p for p in self if not p.restricted}
 
     def append(self, player: Player) -> None:
-        self.update(player)
+        self.enqueue_player(player)
         return super().append(player)
     
     def remove(self, player: Player) -> None:
@@ -88,8 +89,12 @@ class Players(List[Player]):
     def exit(self, player: Player):
         if not player.restricted:
             for p in self:
-                # TODO
-                pass
+                p.handler.enqueue_exit(player)
+
+    def enqueue_player(self, player: Player):
+        for p in self:
+            if p is not player:
+                p.handler.enqueue_player(player)
     
     def enqueue_channel(self, channel):
         for p in self:
