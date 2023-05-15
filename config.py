4b444565f231018fe6ad00258e0fdd9dbf73b385
Lekuru <levijordan007@gmail.com>
2023-05-15 22:39:58 +0200

    Add descriptions

--- config.py
+++ config.py
@@ -8,7 +8,10 @@ REDIS_HOST='127.0.0.1'
 REDIS_PORT=6379
 REDIS_PASS='examplePassword'
 
+# You can set up multiple ports here
 PORTS = [13381, 13382]
 
+# This will be visible inside the menu
+# Example: https://osu.ppy.sh/ss/18600390/1055
 MENUICON_IMAGE=""
 MENUICON_URL=""
