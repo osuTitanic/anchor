5a3433e466501b2b9c56183523d646d960abcc3f
Lekuru <levijordan007@gmail.com>
2023-06-01 23:28:25 +0200

    Move config to .env file

--- config.py
+++ config.py
@@ -1,28 +1,29 @@
 
-POSTGRES_HOST='127.0.0.1'
-POSTGRES_PORT=5432
-POSTGRES_USER='bancho'
-POSTGRES_PASSWORD='examplePassword'
-
-REDIS_HOST='127.0.0.1'
-REDIS_PORT=6379
-REDIS_PASS='examplePassword'
-
-# You can set up multiple ports here
-PORTS = [13381, 13382]
-
-# This will be visible inside the menu
-# Example: https://osu.ppy.sh/ss/18600390/1055
-MENUICON_IMAGE=""
-MENUICON_URL=""
-
-# This will give every player supporter permissions
-FREE_SUPPORTER=True
+import dotenv
+import os
 
 # You can configure all allowed clients here
 # If you don't want to validate the hash, you can leave the value empty
 CLIENT_HASHES = {
-    'b20130606.1': 'f84642c6251bda9b40a3ad3b5ee1a984'
+    'b20130606.1': '93f0c72fdb32fc2cbc8fdf253b741a27'
 }
 
+# This is optional, but it will improve performance
 IP_DATABASE_URL = "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-City.mmdb"
+
+dotenv.load_dotenv(override=False)
+
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
+POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT'))
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
+REDIS_HOST = os.environ.get('REDIS_HOST')
+REDIS_PORT = int(os.environ.get('REDIS_PORT'))
+
+PORTS = eval(os.environ.get('BANCHO_PORTS'))
+
+MENUICON_IMAGE = os.environ.get('BANCHO_MENUICON_IMAGE')
+MENUICON_URL = os.environ.get('BANCHO_MENUICON_URL')
+
+FREE_SUPPORTER = os.environ.get('FREE_SUPPORTER').lower() == 'true'
