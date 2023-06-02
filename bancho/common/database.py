61fedcdb76a8dee237bda2331497c6fdee58d4f5
Levi <84310095+Lekuruu@users.noreply.github.com>
2023-06-02 19:59:26 +0200

    Cleanup for sql queries

--- bancho/common/database.py
+++ bancho/common/database.py
@@ -49,28 +49,45 @@ class Postgres:
             session.close()
 
     def user_by_name(self, name: str) -> Optional[DBUser]:
-        return self.session.query(DBUser).filter(DBUser.name == name).first()
+        return self.session.query(DBUser) \
+                .filter(DBUser.name == name) \
+                .first()
     
     def user_by_id(self, id: int) -> Optional[DBUser]:
-        return self.session.query(DBUser).filter(DBUser.id == id).first()
+        return self.session.query(DBUser) \
+                .filter(DBUser.id == id) \
+                .first()
     
     def beatmap_by_file(self, filename: str) -> Optional[DBBeatmap]:
-        return self.session.query(DBBeatmap).filter(DBBeatmap.filename == filename).first()
+        return self.session.query(DBBeatmap) \
+                .filter(DBBeatmap.filename == filename) \
+                .first()
     
     def beatmap_by_checksum(self, md5: str) -> Optional[DBBeatmap]:
-        return self.session.query(DBBeatmap).filter(DBBeatmap.md5 == md5).first()
+        return self.session.query(DBBeatmap) \
+                .filter(DBBeatmap.md5 == md5) \
+                .first()
     
     def personal_best(self, beatmap_id: int, user_id: int) -> Optional[DBScore]:
-        return self.session.query(DBScore).filter(DBScore.beatmap_id == beatmap_id).filter(DBScore.user_id == user_id).filter(DBScore.status == 3).first()
+        return self.session.query(DBScore) \
+                .filter(DBScore.beatmap_id == beatmap_id) \
+                .filter(DBScore.user_id == user_id) \
+                .filter(DBScore.status == 3) \
+                .first()
     
     def channels(self) -> List[DBChannel]:
         return self.session.query(DBChannel).all()
     
     def stats(self, user_id: int, mode: int) -> Optional[DBStats]:
-        return self.session.query(DBStats).filter(DBStats.user_id == user_id).filter(DBStats.mode == mode).first()
+        return self.session.query(DBStats) \
+                .filter(DBStats.user_id == user_id) \
+                .filter(DBStats.mode == mode) \
+                .first()
     
     def relationships(self, user_id: int) -> List[DBStats]:
-        return self.session.query(DBRelationship).filter(DBRelationship.user_id == user_id).all()
+        return self.session.query(DBRelationship) \
+                .filter(DBRelationship.user_id == user_id) \
+                .all()
     
     def add_relationship(self, user_id: int, target_id: int, friend: bool = True) -> DBRelationship:
         instance = self.session
@@ -87,7 +104,10 @@ class Postgres:
     
     def remove_relationship(self, user_id: int, target_id: int, status: int = 0):
         instance = self.session
-        rel = instance.query(DBRelationship).filter(DBRelationship.user_id == user_id).filter(DBRelationship.target_id == target_id).filter(DBRelationship.status == status)
+        rel = instance.query(DBRelationship) \
+                .filter(DBRelationship.user_id == user_id) \
+                .filter(DBRelationship.target_id == target_id) \
+                .filter(DBRelationship.status == status)
 
         if rel.first():
             rel.delete()
