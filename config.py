669ec7e05d349b64ce8483e09a6d3a6450e216f3
Levi <84310095+Lekuruu@users.noreply.github.com>
2023-06-12 07:40:48 +0000

    Add default arguments

--- config.py
+++ config.py
@@ -14,13 +14,13 @@ IP_DATABASE_URL = "https://github.com/P3TERX/GeoLite.mmdb/raw/download/GeoLite2-
 dotenv.load_dotenv(override=False)
 
 POSTGRES_HOST = os.environ.get('POSTGRES_HOST')
-POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT'))
+POSTGRES_PORT = int(os.environ.get('POSTGRES_PORT', 5432))
 POSTGRES_USER = os.environ.get('POSTGRES_USER')
 POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
 
-PORTS = eval(os.environ.get('BANCHO_PORTS'))
+PORTS = eval(os.environ.get('BANCHO_PORTS', '[13381]'))
 
 MENUICON_IMAGE = os.environ.get('BANCHO_MENUICON_IMAGE')
 MENUICON_URL = os.environ.get('BANCHO_MENUICON_URL')
 
-FREE_SUPPORTER = os.environ.get('FREE_SUPPORTER').lower() == 'true'
+FREE_SUPPORTER = os.environ.get('FREE_SUPPORTER', 'True').lower() == 'true'
