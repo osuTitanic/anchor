fc41c4d3d8151aab787acccd726da589da68c934
Lekuru <levijordan007@gmail.com>
2023-06-23 19:05:56 +0200

    Fix typing

--- bancho/jobs/__init__.py
+++ bancho/jobs/__init__.py
@@ -1,10 +1,10 @@
 
 from concurrent.futures._base import Future
 from concurrent.futures       import ThreadPoolExecutor
-from typing                   import Any, Callable
+from typing                   import Any, Callable, Tuple
 
 class Jobs(ThreadPoolExecutor):
-    def __init__(self, max_workers = None, thread_name_prefix: str = "job", initializer = None, initargs: tuple[Any, ...] = ...) -> None:
+    def __init__(self, max_workers = None, thread_name_prefix: str = "job", initializer = None, initargs: Tuple[Any, ...] = ...) -> None:
         super().__init__(max_workers, thread_name_prefix, initializer, initargs)
 
         from bancho.services import logger
