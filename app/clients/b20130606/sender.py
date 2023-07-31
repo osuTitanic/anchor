59985b642447a4b347b86d9c9cc0efae1f022cd0
Lekuru <contact@lekuru.xyz>
2023-07-31 22:35:28 +0200

    Change 'ResponsePacket' to self.packets

--- app/clients/b20130606/sender.py
+++ app/clients/b20130606/sender.py
@@ -1,17 +1,22 @@
 
-from typing import Optional
+from app.common.objects import UserPresence, UserQuit, UserStats
 from app.clients.sender import BaseSender
 from app.common.streams import StreamOut
 
 from .constants import ResponsePacket
+from .writer import Writer
+
+from typing import List, Optional
 
 class PacketSender(BaseSender):
 
     protocol_version = 18
+    packets = ResponsePacket
+    writer = Writer
 
     def send_login_reply(self, reply: int):
         self.player.send_packet(
-            ResponsePacket.LOGIN_REPLY,
+            self.packets.LOGIN_REPLY,
             int(reply).to_bytes(
                 length=4,
                 byteorder='little',
@@ -21,7 +26,7 @@ class PacketSender(BaseSender):
 
     def send_protocol_version(self, version: int):
         self.player.send_packet(
-            ResponsePacket.PROTOCOL_VERSION,
+            self.packets.PROTOCOL_VERSION,
             int(version).to_bytes(
                 length=4,
                 byteorder='little'
@@ -29,14 +34,14 @@ class PacketSender(BaseSender):
         )
 
     def send_ping(self):
-        self.player.send_packet(ResponsePacket.PING)
+        self.player.send_packet(self.packets.PING)
 
     def send_announcement(self, message: str):
         stream = StreamOut()
         stream.string(message)
 
         self.player.send_packet(
-            ResponsePacket.ANNOUNCE,
+            self.packets.ANNOUNCE,
             stream.get()
         )
 
@@ -50,6 +55,6 @@ class PacketSender(BaseSender):
         )
 
         self.player.send_packet(
-            ResponsePacket.ANNOUNCE,
+            self.packets.ANNOUNCE,
             stream.get()
         )
