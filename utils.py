e079d6ce4e0caf4bebbf2baad2080ebf5905507c
Lekuru <contact@lekuru.xyz>
2023-07-31 02:27:42 +0200

    Add utils

---
+++ utils.py
@@ -0,0 +1,28 @@
+
+from app.common.database import DBScore
+from datetime import datetime
+
+import hashlib
+
+def get_ticks(dt) -> int:
+    dt = dt.replace(tzinfo=None)
+    return int((dt - datetime(1, 1, 1)).total_seconds() * 10000000)
+
+def compute_score_checksum(score: DBScore) -> str:
+    return hashlib.md5(
+        '{}p{}o{}o{}t{}a{}r{}e{}y{}o{}u{}{}{}'.format(
+            (score.n100 + score.n300),
+            score.n50,
+            score.nGeki,
+            score.nKatu,
+            score.nMiss,
+            score.beatmap.md5,
+            score.max_combo,
+            score.perfect,
+            score.user.name,
+            score.total_score,
+            score.grade,
+            score.mods,
+            (not score.failtime) # (passed)
+        ).encode()
+    ).hexdigest()
