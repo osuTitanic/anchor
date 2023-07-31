c505b4accd6af35cee9b69f3965cc8cfcae6e37e
Lekuru <contact@lekuru.xyz>
2023-07-31 19:55:21 +0200

    Implement new methods

--- app/objects/player.py
+++ app/objects/player.py
@@ -1,9 +1,13 @@
 
 from app.protocol import BanchoProtocol, IPAddress
+from app.common.database.repositories import users
 from app.common.database import DBUser, DBStats
 from app.common.constants import PresenceFilter
 from app.clients import BaseReader, BaseWriter
 from app.objects import OsuClient, Status
+
+from twisted.internet.error import ConnectionDone
+from twisted.python.failure import Failure
 
 from typing import Optional, List
 
@@ -40,3 +44,29 @@ class Player(BanchoProtocol):
 
     def __repr__(self) -> str:
         return f'<Player ({self.id})>'
+
+    def connectionLost(self, reason: Failure = Failure(ConnectionDone())):
+        # TODO: Remove from player collection
+        # TODO: Notify other clients
+        # TODO: Remove from channels
+        # TODO: Remove spectator channel from collection
+        # TODO: Remove from match
+        super().connectionLost(reason)
+
+    def reload_object(self) -> DBUser:
+        """Reload player stats from database"""
+        self.object = users.fetch_by_id(self.id)
+        self.stats = self.object.stats
+
+        # TODO: Update leaderboard cache
+
+        return self.object
+
+    def closeConnection(self, error: Optional[Exception] = None):
+        self.connectionLost()
+        super().closeConnection(error)
+
+    def loginReceived(self, username: str, md5: str, client: OsuClient):
+        self.logger.info(f'Login attempt as "{username}" with {client.version.string}.')
+
+        # TODO: Select packet decoder & writer
