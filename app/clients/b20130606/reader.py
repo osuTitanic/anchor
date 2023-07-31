2c93ff3b509791cc73acaa33b7b2353c7b17d6ee
Lekuru <contact@lekuru.xyz>
2023-07-31 17:25:39 +0200

    Implement 'read_match'

--- app/clients/b20130606/reader.py
+++ app/clients/b20130606/reader.py
@@ -113,4 +113,62 @@ class Reader(BaseReader):
         )
 
     def read_match(self) -> Match:
-        pass
+        match_id = self.stream.s16()
+
+        in_progress = self.stream.bool()
+        match_type = MatchType(self.stream.u8())
+        mods = Mods(self.stream.s16())
+
+        name = self.stream.string()
+        password = self.stream.string()
+
+        beatmap_text = self.stream.string()
+        beatmap_id   = self.stream.s32()
+        beatmap_hash = self.stream.string()
+
+        slot_status = [SlotStatus(self.stream.u8()) for _ in range(8)]
+        slot_team   = [SlotTeam(self.stream.u8()) for _ in range(8)]
+        slot_id     = [self.stream.s32() for i in range(8) if slot_status[i] & SlotStatus.HasPlayer.value]
+
+        host_id = self.stream.s32()
+        mode = GameMode(self.stream.u8())
+
+        scoring_type = MatchScoringTypes(self.stream.u8())
+        team_type    = MatchTeamTypes(self.stream.u8())
+
+        freemod = self.stream.bool()
+        slot_mods = [None for _ in range(8)]
+
+        if freemod:
+            slot_mods = [Mods(mods) for mods in range(8)]
+
+        slots = [
+            Slot(
+                slot_id[i],
+                slot_status[i],
+                slot_team[i],
+                slot_mods[i]
+            )
+            for i in range(8)
+        ]
+
+        seed = self.stream.s32()
+
+        return Match(
+            match_id,
+            in_progress,
+            match_type,
+            mods,
+            name,
+            password,
+            beatmap_text,
+            beatmap_id,
+            beatmap_hash,
+            slots,
+            host_id,
+            mode,
+            scoring_type,
+            team_type,
+            freemod,
+            seed
+        )
