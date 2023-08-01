f86bb00bc51208deff3b1860ada76e991f494fdc
Lekuru <contact@lekuru.xyz>
2023-08-02 00:39:41 +0200

    Add "PacketEnum" class

--- app/clients/packets.py
+++ app/clients/packets.py
@@ -1,12 +1,21 @@
 
 from typing import Dict, Callable, Tuple
-from enum import Enum
+from enum import IntEnum
+
+class PacketEnum(IntEnum):
+    ...
+
+    def __eq__(self, other: IntEnum) -> bool:
+        return self.name == other.name
+
+    def __hash__(self) -> int:
+        return hash(self.name)
 
 PACKETS: Dict[
     int,
     Tuple[
-        Dict[Enum, Callable], # RequestPackets
-        Dict[Enum, Callable]  # ResponsePackets
+        Dict[PacketEnum, Callable], # RequestPackets
+        Dict[PacketEnum, Callable]  # ResponsePackets
     ]
 ] = {
     2013606: ({}, {})
