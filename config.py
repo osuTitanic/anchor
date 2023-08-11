27d6ff62209337a537f48454dd5a7b4351092150
Lekuru <contact@lekuru.xyz>
2023-08-11 16:06:16 +0200

    Add autojoin channel configuration

--- config.py
+++ config.py
@@ -16,6 +16,8 @@ S3_BASEURL    = os.environ.get('S3_BASEURL')
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = int(os.environ.get('REDIS_PORT', 6379))
 
+AUTOJOIN_CHANNELS = eval(os.environ.get('AUTOJOIN_CHANNELS', "['#osu', '#announce']"))
+
 PORTS = eval(os.environ.get('BANCHO_PORTS', '[13381, 13382, 13383]'))
 
 DOMAIN_NAME = os.environ.get('DOMAIN_NAME')
