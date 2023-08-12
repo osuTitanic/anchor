a2e80ce737ff317e99108532751f24b7b4eb31d2
Lekuru <contact@lekuru.xyz>
2023-08-12 04:00:15 +0200

    Fix executor shutdown

--- app/server.py
+++ app/server.py
@@ -49,7 +49,7 @@ class BanchoFactory(Factory):
         app.session.logger.warning(f'Stopping factory: {self}')
         app.session.events.submit('shutdown')
         app.session.jobs.shutdown(cancel_futures=True)
-        app.session.executor.shutdown(cancel_futures=True)
+        app.session.executor.shutdown(cancel_futures=True, wait=False)
 
         for player in app.session.players:
             status.delete(player.id)
