c0b1c64f221c3b4d12eb29856110e6780a2dcbd9
Lekuru <contact@lekuru.xyz>
2023-09-23 18:20:52 +0200

    Add logging

--- app/objects/player.py
+++ app/objects/player.py
@@ -552,6 +552,7 @@ class Player(BanchoProtocol):
         if self.object.country == 'XX':
             # User is logging in for the first time
             # Update their country value in the database
+            self.logger.info('Updating country...')
             self.object.country = self.client.ip.country_code.upper()
             users.update(
                 user_id=self.id,
