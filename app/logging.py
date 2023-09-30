4250f921b5a7befb231df17308f1e6f195ab7117
Lekuru <contact@lekuru.xyz>
2023-09-30 15:45:33 +0200

    Implement database logging

--- app/logging.py
+++ app/logging.py
@@ -1,14 +1,18 @@
 
+from app.common.database.repositories import logs
+
 from datetime import datetime
 from logging import Formatter, StreamHandler, FileHandler
 from logging import (
     CRITICAL,
     WARNING,
+    NOTSET,
     ERROR,
     DEBUG,
     INFO
 )
 
+import traceback
 import os
 
 class ColorFormatter(Formatter):
@@ -25,13 +29,37 @@ class ColorFormatter(Formatter):
 
     FORMATS = {
         DEBUG:    grey + format_prefix            + format + reset,
+        NOTSET:   grey + format_prefix            + format + reset,
         INFO:     grey + format_prefix + cyan     + format + reset,
         WARNING:  grey + format_prefix + yellow   + format + reset,
         ERROR:    grey + format_prefix + red      + format + reset,
-        CRITICAL: grey + format_prefix + bold_red + format + reset
+        CRITICAL: grey + format_prefix + bold_red + format + reset,
     }
 
+    STRINGS = {
+        DEBUG:    'debug',
+        INFO:     'info',
+        WARNING:  'warning',
+        ERROR:    'error',
+        CRITICAL: 'critical',
+        NOTSET:   'notset'
+    }
+
+    DB_LEVEL = WARNING
+
     def format(self, record):
+        try:
+            if record.levelno >= self.DB_LEVEL:
+                # Submit to db
+                logs.create(
+                    message=record.getMessage(),
+                    level=self.STRINGS.get(record.levelno),
+                    type=record.name
+                )
+        except Exception as e:
+            traceback.print_exc()
+            print(f'Failed to submit log to database: {e}')
+
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = Formatter(log_fmt)
         return formatter.format(record)
