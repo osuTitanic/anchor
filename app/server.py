fde1aaad4241029feb7bcd942314a59807fd9752
Lekuru <contact@lekuru.xyz>
2023-11-04 15:37:52 +0100

    Fix `AlreadyQuit` exception

--- app/server.py
+++ app/server.py
@@ -65,7 +65,6 @@ class BanchoFactory(Factory):
 
         app.session.events.submit('shutdown')
         app.session.jobs.shutdown(cancel_futures=True, wait=False)
-        app.session.pool.stop()
 
     def buildProtocol(self, addr: IAddress) -> Optional[Protocol]:
         client = self.protocol(addr)
