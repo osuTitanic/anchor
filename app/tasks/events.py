f3bfbfc875b21ab55cd8c967e0b96cdbc27de65d
Lekuru <contact@lekuru.xyz>
2025-03-23 13:30:25 +0100

    Switch back to listener event model because things don't seem to work :shrug:

--- app/tasks/events.py
+++ app/tasks/events.py
@@ -4,10 +4,5 @@ import app
 @app.session.tasks.submit(interval=1, threaded=True)
 def event_listener() -> None:
     """This will listen for redis pubsub events and call the appropriate functions."""
-    message = app.session.events.poll()
-
-    if not message:
-        return
- 
-    func, args, kwargs = message
-    func(*args, **kwargs)
+    for func, args, kwargs in app.session.events.listen():
+        func(*args, **kwargs)
