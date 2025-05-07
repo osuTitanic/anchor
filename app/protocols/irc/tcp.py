a538c9335bba0d268f33a704785f90471f07a547
Lekuru <contact@lekuru.xyz>
2025-05-07 19:34:41 +0200

    Change login timeout duration

--- app/protocols/irc/tcp.py
+++ app/protocols/irc/tcp.py
@@ -30,8 +30,8 @@ class TcpIrcProtocol(IrcClient, IRC):
             self.enqueue_error("IRC connections have been disabled. Please check back later!")
             self.close_connection('IRC is disabled')
 
-        # Ensure client is logged in after 90 seconds, else close connection
-        reactor.callLater(5, self.handle_timeout_callback)
+        # Ensure client is logged in after 15 seconds, else close connection
+        reactor.callLater(15, self.handle_timeout_callback)
 
     def connectionLost(self, reason: Failure) -> None:
         self.logger.info(
