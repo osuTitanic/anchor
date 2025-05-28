0bf54b223b5fae06741b1eaed8d9e8970f496ebc
Lekuru <contact@lekuru.xyz>
2025-05-28 18:24:45 +0200

    Make hostname, username & realname args optional

--- app/handlers/irc/authentication.py
+++ app/handlers/irc/authentication.py
@@ -11,9 +11,9 @@ def handle_user_command(
     client: IrcClient,
     prefix: str,
     username: str,
-    hostname: str,
-    servername: str,
-    realname: str
+    hostname: str | None = None,
+    servername: str | None = None,
+    realname: str | None = None
 ) -> None:
     client.is_osu = (
         (realname == "osu" or realname.isdigit()) and
