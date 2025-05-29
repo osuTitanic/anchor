3e8323c2972fb6ed9691347288d218294e9fbe16
Lekuru <contact@lekuru.xyz>
2025-05-29 20:07:06 +0200

    Fix message validation

--- app/objects/channel.py
+++ app/objects/channel.py
@@ -254,6 +254,8 @@ class Channel:
             sender.silence(60 * 5, "Auto-silenced for using bad words in chat.")
             officer.call(f'Message: {message}')
             return False
+        
+        return True
 
     def handle_external_message(
         self,
