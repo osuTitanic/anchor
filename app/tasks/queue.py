06f194e993698e4ee4a4b63fd3b1a98670905571
Lekuru <contact@lekuru.xyz>
2025-06-26 22:05:21 +0200

    Defer timeouts to thread so that queue can continue cooking

--- app/tasks/queue.py
+++ app/tasks/queue.py
@@ -9,17 +9,17 @@ def execute_task_queue():
     """
     while True:
         try:
+            if len(tasks.do_later_futures) >= tasks.do_later_workers:
+                # Wait for first future to complete, cancel it if necessary
+                future = tasks.do_later_futures.pop(0)
+                tasks.defer_to_thread(future.result, timeout=120)
+
             # Get the latest task, sorted by priority
             _, _, func, args, kwargs = tasks.do_later_queue.get()
 
             # Submit task to executor
             future = tasks.do_later_executor.submit(func, *args, **kwargs)
             tasks.do_later_futures.append(future)
-
-            if len(tasks.do_later_futures) >= tasks.do_later_workers:
-                # Wait for first future to complete, cancel it if necessary
-                future = tasks.do_later_futures.pop(0)
-                future.result(timeout=120)
         except Exception as e:
             officer.call(f"Failed to execute '{func.__name__}'.", exc_info=e)
         finally:
