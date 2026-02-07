f6c0acc410da6df82a426f7ecb766b621440e127
Lekuru <contact@lekuru.xyz>
2026-02-07 19:54:00 +0100

    Use `bytearray` directly instead of converting it into bytes

--- app/protocols/osu/streams.py
+++ app/protocols/osu/streams.py
@@ -11,35 +11,34 @@ class ByteStream:
         self.offset = 0
         self.buffer = bytearray()
 
-    @property
-    def data(self) -> bytes:
-        return bytes(self.buffer)
+    def __repr__(self):
+        return self.buffer.__repr__()
 
-    def __add__(self, data: bytes) -> "ByteStream":
+    def __len__(self):
+        return len(self.buffer)
+
+    def __iadd__(self, data: bytes) -> "ByteStream":
         self.append(data)
         return self
-
-    def __repr__(self):
-        return f"{bytes(self.buffer)}"
 
     def write(self, data: bytes) -> None:
         self.client.enqueue(data)
 
-    def read(self, size: int = -1) -> bytes:
+    def read(self, size: int = -1) -> bytearray:
         if size < 0:
             return self.readall()
 
         if self.offset + size > len(self.buffer):
             raise OverflowError(f"{size} exceeds available data {len(self.buffer) - self.offset}")
 
-        data = bytes(self.buffer[self.offset:self.offset + size])
+        result = self.buffer[self.offset:self.offset + size]
         self.offset += size
-        return data
+        return result
 
-    def readall(self) -> bytes:
-        data = bytes(self.buffer[self.offset:])
+    def readall(self) -> bytearray:
+        result = self.buffer[self.offset:]
         self.offset = len(self.buffer)
-        return data
+        return result
 
     def append(self, data: bytes) -> None:
         self.buffer.extend(data)
@@ -60,7 +59,6 @@ class ByteStream:
 
     def count(self, byte: bytes) -> int:
         return self.buffer.count(byte)
-    
-    def split(self, byte: bytes, maxsplit: int = -1) -> tuple[bytes, ...]:
-        parts = bytes(self.buffer).split(byte, maxsplit)
-        return parts
+
+    def split(self, byte: bytes, maxsplit: int = -1) -> tuple[bytearray, ...]:
+        return self.buffer.split(byte, maxsplit)
